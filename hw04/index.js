2f17242dcab6c1ae593203860e184528bd6f112f
Tuananh15062001 <tuananh15062001@gmail.com>
2023-07-01 14:34:14 +0700

    Btvn Buoi4

---
+++ hw04/index.js
@@ -0,0 +1,16 @@
+// Câu 2:
+let n = 3;
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+  sum += 1/i;
+}
+console.log(sum.toFixed(2));
+
+// Câu 3:
+let a = 3
+let result = 0
+for (let j = 0; j <= a; j++) {
+    result += j/(j+1)
+}
+console.log(result.toFixed(2))
+
